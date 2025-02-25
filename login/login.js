322f01dfec89cbbfff3bfdaa5120e69faa418b07
Christofer Jhon <chrisjhon217@gmail.com>
2025-02-24 21:54:02 -0500

    Primer commit PaginaICH

---
+++ login/login.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const users = [
+        { username: "admin", password: "1234", redirectTo: "" },
+        { username: "usuario", password: "5678", redirectTo: "../index.html"} 
+    ];
+
+    if (window.location.pathname.includes("/login/login.html")) {
+        document.getElementById("loginForm").addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            let username = document.getElementById("username").value;
+            let password = document.getElementById("password").value;
+            let errorMsg = document.getElementById("error-msg");
+
+            let validUser = users.find(user => user.username === username && user.password === password);
+
+            if (validUser) {
+                localStorage.setItem("loggedIn", "true");
+                localStorage.setItem("user", username); // Guardar usuario en localStorage
+                window.location.href = validUser.redirectTo; // 🔥 Redirigir según el usuario
+            }  else {
+                errorMsg.textContent = "Usuario o contraseña incorrectos.";
+            }
+        });
+    } else {
+        if (localStorage.getItem("loggedIn") !== "true") {
+            window.location.href = "login/login.html"; // 🔄 Ajustar ruta de login
+        }
+    }
+});
